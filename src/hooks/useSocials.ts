739411f481cff072f1e227b799804dc2f229cf6e
Anas <anasalwardtech@gmail.com>
2026-04-09 21:39:51 +0300

    refactor: modularize hooks by feature and update imports accordingly

--- src/hooks/useSocials.ts
+++
@@ -1,9 +0,0 @@
-import { useSupabaseQuery } from './useSupabase';
-import { Contact } from '../types';
-
-/**
- * Hook to fetch projects from Supabase
- */
-export function useSocials(params?: Record<string, any>) {
-    return useSupabaseQuery<Contact>(['socials'], 'socials', params);
-}
