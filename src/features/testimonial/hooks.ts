f1e359d7bcf7249fa793ecc28c2479a40b3d1052
Anas <anasalwardtech@gmail.com>
2026-04-09 21:39:35 +0300

    refactor: migrate feature-specific hooks to their respective feature directories and update imports accordingly

--- src/features/testimonial/hooks.ts
+++ src/features/testimonial/hooks.ts
@@ -1,9 +1,10 @@
-import { useSupabaseQuery, useSupabasePaginatedQuery } from './useSupabase';
-import { Testimonial } from '../types';
-import {useSettings} from './useSettings'
-import { SECTION, SETTINGS_TYPE } from '../types';
+import { useSupabaseQuery, useSupabasePaginatedQuery } from '@/hooks/useSupabase';
+import { Testimonial } from '@/types';
+import { useSettings } from '@/features/settings/hooks'
+import { SECTION, SETTINGS_TYPE } from '@/types';
+
 /**
- * Hook to fetch skills from Supabase
+ * Hook to fetch testimonials from Supabase
  */
 export function useTestimonials(params?: Record<string, any>) {
     return useSupabaseQuery<Testimonial>(['testimonials'], 'testimonials', params);
@@ -16,5 +17,5 @@ export function usePaginatedTestimonials(page: number = 1, params?: Record<strin
     const DEFAULT_PAGE_SIZE = 3
     const { data: settings } = useSettings({ section: SECTION.TESTIMONIALS, type: SETTINGS_TYPE.PAGINATION })
     const pageSize = settings?.PAGE_SIZE ? Number(settings.PAGE_SIZE) : DEFAULT_PAGE_SIZE;
-    return {...useSupabasePaginatedQuery<Testimonial>(['testimonials', 'paginated'], 'testimonials', page, pageSize, params), pageSize};
+    return { ...useSupabasePaginatedQuery<Testimonial>(['testimonials', 'paginated'], 'testimonials', page, pageSize, params), pageSize };
 }
